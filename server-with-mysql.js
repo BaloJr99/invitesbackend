2a59f12560b0fea8128113580d111eed0cd9a36a
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2024-03-05 22:44:08 -0600

    Fix api

--- server-with-mysql.js
+++ server-with-mysql.js
@@ -1,5 +1,6 @@
 import { createApp } from './app.js'
 import { EntriesModel } from './databases/mysql/entries.js'
+import { EventsModel } from './databases/localhost/events.js'
 import { UserModel } from './databases/mongodb/users.js'
 
-createApp({ entryModel: EntriesModel, userModel: UserModel })
+createApp({ entryModel: EntriesModel, userModel: UserModel, eventModel: EventsModel })
