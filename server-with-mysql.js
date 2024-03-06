9985fd3ff252557b31cf7ed6b1b0938d06a782bc
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2024-03-06 12:44:48 -0600

    Fixed issue with server

--- server-with-mysql.js
+++ server-with-mysql.js
@@ -1,6 +1,6 @@
 import { createApp } from './app.js'
 import { EntriesModel } from './databases/mysql/entries.js'
-import { EventsModel } from './databases/localhost/events.js'
+import { EventsModel } from './databases/mysql/events.js'
 import { UserModel } from './databases/mongodb/users.js'
 
 createApp({ entryModel: EntriesModel, userModel: UserModel, eventModel: EventsModel })
