9093027f2a152b86b15dddff36c54b7eaea7da10
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2024-03-24 12:18:47 -0600

    Fix for serverside image databases

--- server-with-mysql.js
+++ server-with-mysql.js
@@ -1,7 +1,16 @@
 import { createApp } from './app.js'
+import { ImagesModel } from './databases/cloudinary/images.js'
 import { EntriesModel } from './databases/mysql/entries.js'
 import { EventsModel } from './databases/mysql/events.js'
 import { FamilyGroupModel } from './databases/mysql/familyGroups.js'
+import { InviteImagesModel } from './databases/localhost/inviteImages.js'
 import { UserModel } from './databases/mongodb/users.js'
 
-createApp({ entryModel: EntriesModel, userModel: UserModel, eventModel: EventsModel, familyGroupModel: FamilyGroupModel })
+createApp({
+  entryModel: EntriesModel,
+  userModel: UserModel,
+  eventModel: EventsModel,
+  familyGroupModel: FamilyGroupModel,
+  imagesModel: ImagesModel,
+  inviteImagesModel: InviteImagesModel
+})
