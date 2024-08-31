42ce33b8e4cf59700826172ff8c72929ebacc6a3
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2024-08-30 23:15:10 -0600

    Provide logs from the last 31 days

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -26,8 +26,12 @@ export class LoggerService {
   }
 
   getLogs = async () => {
+    const todayMinus31 = new Date();
+    todayMinus31.setDate(todayMinus31.getDate() - 31);
+
     const [results] = await this.connection.query(
-      'SELECT BIN_TO_UUID(id) AS id, dateOfError, customError, exceptionMessage, CAST(userId AS CHAR) AS userId FROM errorLogs',
+      'SELECT BIN_TO_UUID(id) AS id, dateOfError, customError, exceptionMessage, CAST(userId AS CHAR) AS userId FROM errorLogs WHERE dateOfError >= ?',
+      [ todayMinus31.toISOString().substring(0, 10) ]
     ) as [RowDataPacket[], FieldPacket[]];
 
     return results;
