86ff2b6bf0145dcdee159f5083d63a876b64e848
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2024-11-25 21:08:11 -0600

    Isolating dbName

--- src/config/mongo/mongo.ts
+++ src/config/mongo/mongo.ts
@@ -2,7 +2,9 @@ import { connect } from 'mongoose'
 
 async function dbConnect(): Promise<void> {
   const DB_URI = process.env.MONGO_URI
-  await connect(DB_URI)
+  await connect(DB_URI, {
+    dbName: process.env.MONGO_DB
+  })
 }
 
 export default dbConnect
