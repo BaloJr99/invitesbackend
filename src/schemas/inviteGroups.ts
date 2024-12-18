ba4d920a26561043d4045798891bfc8b74a99fec
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2024-12-17 22:41:13 -0600

    Updated sweet xv model

--- src/schemas/inviteGroups.ts
+++ src/schemas/inviteGroups.ts
@@ -2,18 +2,22 @@ import { z } from 'zod'
 import { IInviteGroup } from '../interfaces/inviteGroupsModel.js'
 
 const inviteGroupSchema = z.object({
-  inviteGroup: z.string({
-    invalid_type_error: 'The invite group name must be a string',
-    required_error: 'The invite group is required'
-  }).min(1, {
-    message: 'You must provide the invite group name'
-  }),
-  eventId: z.string({
-    invalid_type_error: 'The event id must be a string',
-    required_error: 'The event id is required'
-  }).uuid({
-    message: 'The event id should be a uuid type'
-  })
+  inviteGroup: z
+    .string({
+      invalid_type_error: 'The invite group name must be a string',
+      required_error: 'The invite group is required'
+    })
+    .min(1, {
+      message: 'You must provide the invite group name'
+    }),
+  eventId: z
+    .string({
+      invalid_type_error: 'The event id must be a string',
+      required_error: 'The event id is required'
+    })
+    .uuid({
+      message: 'The event id should be a uuid type'
+    })
 })
 
 export function validateInviteGroup(inviteGroup: IInviteGroup) {
--- src/schemas/settings.ts
+++ src/schemas/settings.ts
@@ -1,123 +1,179 @@
 import { z } from 'zod'
 import { ISweetXvSettings } from '../interfaces/settingsModel.js'
 
-const sweetXvSettingsSchema = z.object({
-  eventId: z.string().uuid({
-    message: 'Invalid UUID'
-  }),
-  primaryColor: z
-    .string({
-      invalid_type_error: 'The primary color must be a string',
-      required_error: 'The primary color is required'
-    })
-    .min(7, {
-      message: 'You must provide a valid hex color code'
+const sweetXvSettingsSchema = z
+  .object({
+    sections: z
+      .object({
+        sectionId: z.string({
+          invalid_type_error: 'The section id must be a string',
+          message: 'You must provide the section id'
+        }),
+        selected: z.boolean({
+          invalid_type_error: 'The selected must be a boolean',
+          message: 'You must provide the selected value'
+        })
+      })
+      .array(),
+    eventId: z.string().uuid({
+      message: 'Invalid UUID'
     }),
-  secondaryColor: z
-    .string({
-      invalid_type_error: 'The secondary color must be a string',
-      required_error: 'The secondary color is required'
-    })
-    .min(7, {
-      message: 'You must provide a valid hex color code'
-    }),
-  parents: z
-    .string({
-      invalid_type_error: 'The parents must be a string',
-      required_error: 'The parents is required'
-    })
-    .min(1, {
-      message: 'You must provide the parents names'
-    }),
-  godParents: z
-    .string({
-      invalid_type_error: 'The godParents must be a string',
-      required_error: 'The godParents is required'
-    })
-    .min(1, {
-      message: 'You must provide the god parents names'
-    }),
-  firstSectionSentences: z
-    .string({
-      invalid_type_error: 'The first section sentence must be a string',
-      required_error: 'The first section sentence is required'
-    })
-    .min(1, {
-      message: 'You must provide the first section sentences'
-    }),
-  secondSectionSentences: z
-    .string({
-      invalid_type_error: 'The second section sentence must be a string',
-      required_error: 'The second section sentence is required'
-    })
-    .min(1, {
-      message: 'You must provide the second section sentences'
-    }),
-  massUrl: z
-    .string({
-      invalid_type_error: 'The mass url must be a string',
-      required_error: 'The mass url is required'
-    })
-    .url({
-      message: 'The mass url must be a valid URL'
-    }),
-  massTime: z
-    .string({
-      invalid_type_error: 'The mass time must be a string',
-      required_error: 'The mass time is required'
-    })
-    .time({
-      message: 'Invalid time format'
-    }),
-  massAddress: z
-    .string({
-      invalid_type_error: 'The mass address must be a string',
-      required_error: 'The mass address is required'
-    })
-    .min(1, {
-      message: 'You must provide the mass address'
-    }),
-  receptionUrl: z
-    .string({
-      invalid_type_error: 'The reception url must be a string',
-      required_error: 'The reception url is required'
-    })
-    .url({
-      message: 'The mass url must be a valid URL'
-    }),
-  receptionTime: z
-    .string({
-      invalid_type_error: 'The reception time must be a string',
-      required_error: 'The reception time is required'
-    })
-    .time({
-      message: 'Invalid time format'
-    }),
-  receptionPlace: z
-    .string({
-      invalid_type_error: 'The reception place must be a string',
-      required_error: 'The reception place is required'
-    })
-    .min(1, {
-      message: 'You must provide the reception place'
-    }),
-  receptionAddress: z
-    .string({
-      invalid_type_error: 'The reception address must be a string',
-      required_error: 'The reception address is required'
-    })
-    .min(1, {
-      message: 'You must provide the reception address'
-    }),
-  dressCodeColor: z
-    .string({
-      invalid_type_error: 'The dress code must be a string',
-      required_error: 'The dress code is required'
-    })
-    .min(1, {
-      message: 'You must provide the dress code'
-    })
-})
+    primaryColor: z
+      .string({
+        invalid_type_error: 'The primary color must be a string',
+        required_error: 'The primary color is required'
+      })
+      .min(7, {
+        message: 'You must provide a valid hex color code'
+      }),
+    secondaryColor: z
+      .string({
+        invalid_type_error: 'The secondary color must be a string',
+        required_error: 'The secondary color is required'
+      })
+      .min(7, {
+        message: 'You must provide a valid hex color code'
+      }),
+    parents: z
+      .string({
+        invalid_type_error: 'The parents must be a string',
+        required_error: 'The parents is required'
+      })
+      .min(1, {
+        message: 'You must provide the parents names'
+      })
+      .optional(),
+    godParents: z
+      .string({
+        invalid_type_error: 'The godParents must be a string',
+        required_error: 'The godParents is required'
+      })
+      .min(1, {
+        message: 'You must provide the god parents names'
+      })
+      .optional(),
+    firstSectionSentences: z
+      .string({
+        invalid_type_error: 'The first section sentence must be a string',
+        required_error: 'The first section sentence is required'
+      })
+      .min(1, {
+        message: 'You must provide the first section sentences'
+      }),
+    secondSectionSentences: z
+      .string({
+        invalid_type_error: 'The second section sentence must be a string',
+        required_error: 'The second section sentence is required'
+      })
+      .min(1, {
+        message: 'You must provide the second section sentences'
+      })
+      .optional(),
+    massUrl: z
+      .string({
+        invalid_type_error: 'The mass url must be a string',
+        required_error: 'The mass url is required'
+      })
+      .url({
+        message: 'The mass url must be a valid URL'
+      })
+      .optional(),
+    massTime: z
+      .string({
+        invalid_type_error: 'The mass time must be a string',
+        required_error: 'The mass time is required'
+      })
+      .time({
+        message: 'Invalid time format'
+      })
+      .optional(),
+    massAddress: z
+      .string({
+        invalid_type_error: 'The mass address must be a string',
+        required_error: 'The mass address is required'
+      })
+      .min(1, {
+        message: 'You must provide the mass address'
+      })
+      .optional(),
+    receptionUrl: z
+      .string({
+        invalid_type_error: 'The reception url must be a string',
+        required_error: 'The reception url is required'
+      })
+      .url({
+        message: 'The mass url must be a valid URL'
+      })
+      .optional(),
+    receptionTime: z
+      .string({
+        invalid_type_error: 'The reception time must be a string',
+        required_error: 'The reception time is required'
+      })
+      .time({
+        message: 'Invalid time format'
+      })
+      .optional(),
+    receptionPlace: z
+      .string({
+        invalid_type_error: 'The reception place must be a string',
+        required_error: 'The reception place is required'
+      })
+      .min(1, {
+        message: 'You must provide the reception place'
+      })
+      .optional(),
+    receptionAddress: z
+      .string({
+        invalid_type_error: 'The reception address must be a string',
+        required_error: 'The reception address is required'
+      })
+      .min(1, {
+        message: 'You must provide the reception address'
+      })
+      .optional(),
+    dressCodeColor: z
+      .string({
+        invalid_type_error: 'The dress code must be a string',
+        required_error: 'The dress code is required'
+      })
+      .min(1, {
+        message: 'You must provide the dress code'
+      })
+      .optional()
+  })
+  .superRefine((data, ctx) => {
+    const sections = data.sections
+    let sectionHasIssues = false
+
+    // We need to mark fields as optional if the section is not selected
+    sectionHasIssues =
+      sectionHasIssues ||
+      (sections.some((s) => s.sectionId === 'ceremonyInfo' && s.selected) &&
+        (!data.massUrl ||
+          !data.massTime ||
+          !data.massAddress ||
+          !data.parents ||
+          !data.godParents ||
+          !data.secondSectionSentences)) ||
+      (sections.some((s) => s.sectionId === 'receptionInfo' && s.selected) &&
+        (!data.receptionUrl ||
+          !data.receptionTime ||
+          !data.receptionPlace ||
+          !data.receptionAddress)) ||
+      (sections.some((s) => s.sectionId === 'dressCode' && s.selected) &&
+        !data.dressCodeColor)
+
+    if (sectionHasIssues) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Sections are invalid',
+        path: ['sections']
+      })
+    }
+
+    return sectionHasIssues
+  })
 
 const saveTheDateSettingsSchema = z.object({
   eventId: z.string().uuid({
